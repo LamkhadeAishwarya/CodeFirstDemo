2600f617b8655eb6a31cc1695928fe80e27c06f2
LamkhadeAishwarya <ashwinilamkhade2020@gmail.com>
2023-10-30 16:16:26 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodeFirstDemo.Models;
+using Microsoft.Identity.Client;
+
+namespace CodeFirstDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> op) : base(op)
+        {
+
+        }
+        public DbSet <Employee > Employees {get;set;}
+    }
+}
